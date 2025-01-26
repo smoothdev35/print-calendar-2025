6f881996d3bc82820b28b1cc9b376676aa609307
Hugo Cédric Priet <hugo.priet@freshconsulting.com>
2025-01-26 07:20:29 -0600

    fix: removed eslint rule conflicting with shadcdn ui component

--- eslint.config.js
+++ eslint.config.js
@@ -19,7 +19,6 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/consistent-type-imports': 'error',
     },
