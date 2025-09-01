eb5a4e4798d101f9b1f4231b1f992e143a1dd539
Hugo Priet <hugopriet@Hugos-MacBook-Pro.local>
2025-08-31 21:47:10 -0600

    fix: changed double to single quotyes to fix diagram rendering error in readme

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -1,17 +1,17 @@
-import { type NewEvent, type Event } from '@/models/shared.models';
+import { type NewEvent, type Event } from '@/models/shared.models'
 
 // This is a placeholder for the actual API call.
 // We will replace this with a real API call to the server.
 export const createEvent = async (event: NewEvent): Promise<Event> => {
-  console.log('Creating event:', event);
+  console.log('Creating event:', event)
   // Simulate an API call
-  await new Promise(resolve => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 1000))
 
   // Simulate a successful response from the server
   const newEvent: Event = {
     ...event,
     id: new Date().toISOString(), // a temporary id
-  };
+  }
 
-  return newEvent;
-};
+  return newEvent
+}
--- src/store/calendarStore.tsx
+++ src/store/calendarStore.tsx
@@ -1,26 +1,31 @@
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
-import { v4 as uuidv4 } from 'uuid';
-import { MONTHS } from '@/enums/shared.enums';
-import { type Event, type InteractiveDay, type TMonths, type NewEvent } from '@/models/shared.models';
-import { getCleanCalendarDays } from '@/helpers/shared.helpers';
-import { currentMonth } from '@/lib/utils';
-import { createEvent as createEventAPI } from '@/services/eventService';
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+import { v4 as uuidv4 } from 'uuid'
+import { MONTHS } from '@/enums/shared.enums'
+import {
+  type Event,
+  type InteractiveDay,
+  type TMonths,
+  type NewEvent,
+} from '@/models/shared.models'
+import { getCleanCalendarDays } from '@/helpers/shared.helpers'
+import { currentMonth } from '@/lib/utils'
+import { createEvent as createEventAPI } from '@/services/eventService'
 
 type InteractiveMonth = {
-  month: TMonths;
-  days: InteractiveDay[];
-};
+  month: TMonths
+  days: InteractiveDay[]
+}
 
 type CalendarState = {
-  selectedMonth: TMonths;
-  interactiveCalendar: InteractiveMonth[];
-  setSelectedMonth: (month: TMonths) => void;
-  setInteractiveCalendar: (calendar: InteractiveMonth[]) => void;
-  addEvent: (event: Event, selectedDate: string, selectedMonth: TMonths) => void;
-  deleteEvent: (eventId: string, selectedMonth: TMonths) => void;
-  createEvent: (event: NewEvent, selectedDate: string, selectedMonth: TMonths) => Promise<void>;
-};
+  selectedMonth: TMonths
+  interactiveCalendar: InteractiveMonth[]
+  setSelectedMonth: (month: TMonths) => void
+  setInteractiveCalendar: (calendar: InteractiveMonth[]) => void
+  addEvent: (event: Event, selectedDate: string, selectedMonth: TMonths) => void
+  deleteEvent: (eventId: string, selectedMonth: TMonths) => void
+  createEvent: (event: NewEvent, selectedDate: string, selectedMonth: TMonths) => Promise<void>
+}
 
 export const useCalendarStore = create<CalendarState>()(
   persist(
@@ -36,96 +41,91 @@ export const useCalendarStore = create<CalendarState>()(
         set((state) => {
           const monthIndex = state.interactiveCalendar.findIndex(
             ({ month }) => month === selectedMonth
-          );
+          )
 
-          if (monthIndex === -1) return state;
+          if (monthIndex === -1) return state
 
           const dayIndex = state.interactiveCalendar[monthIndex].days.findIndex(
             ({ date }) => date === selectedDate
-          );
+          )
 
-          if (dayIndex === -1) return state;
+          if (dayIndex === -1) return state
 
-          const newInteractiveCalendar = [...state.interactiveCalendar];
+          const newInteractiveCalendar = [...state.interactiveCalendar]
           const updatedDay = {
             ...newInteractiveCalendar[monthIndex].days[dayIndex],
-            events: [
-              ...newInteractiveCalendar[monthIndex].days[dayIndex].events,
-              event,
-            ],
-          };
+            events: [...newInteractiveCalendar[monthIndex].days[dayIndex].events, event],
+          }
 
-          newInteractiveCalendar[monthIndex].days[dayIndex] = updatedDay;
+          newInteractiveCalendar[monthIndex].days[dayIndex] = updatedDay
 
-          return { interactiveCalendar: newInteractiveCalendar };
-        });
+          return { interactiveCalendar: newInteractiveCalendar }
+        })
       },
       deleteEvent: (eventId: string, selectedMonth: TMonths) => {
         set((state) => {
           const monthIndex = state.interactiveCalendar.findIndex(
             ({ month }) => month === selectedMonth
-          );
+          )
 
-          if (monthIndex === -1) return state;
+          if (monthIndex === -1) return state
 
-          const newInteractiveCalendar = [...state.interactiveCalendar];
-          const month = newInteractiveCalendar[monthIndex];
+          const newInteractiveCalendar = [...state.interactiveCalendar]
+          const month = newInteractiveCalendar[monthIndex]
 
           const dayIndex = month.days.findIndex((day) =>
             day.events.some((event) => event.id === eventId)
-          );
+          )
 
-          if (dayIndex === -1) return state;
+          if (dayIndex === -1) return state
 
           const updatedDay = {
             ...month.days[dayIndex],
-            events: month.days[dayIndex].events.filter(
-              (event) => event.id !== eventId
-            ),
-          };
+            events: month.days[dayIndex].events.filter((event) => event.id !== eventId),
+          }
 
-          newInteractiveCalendar[monthIndex].days[dayIndex] = updatedDay;
+          newInteractiveCalendar[monthIndex].days[dayIndex] = updatedDay
 
-          return { interactiveCalendar: newInteractiveCalendar };
-        });
+          return { interactiveCalendar: newInteractiveCalendar }
+        })
       },
       createEvent: async (event: NewEvent, selectedDate: string, selectedMonth: TMonths) => {
-        const tempId = uuidv4();
-        const tempEvent: Event = { ...event, id: tempId };
+        const tempId = uuidv4()
+        const tempEvent: Event = { ...event, id: tempId }
 
-        get().addEvent(tempEvent, selectedDate, selectedMonth);
+        get().addEvent(tempEvent, selectedDate, selectedMonth)
 
         try {
-          const newEvent = await createEventAPI(event);
+          const newEvent = await createEventAPI(event)
           set((state) => {
             const monthIndex = state.interactiveCalendar.findIndex(
               ({ month }) => month === selectedMonth
-            );
+            )
 
-            if (monthIndex === -1) return state;
+            if (monthIndex === -1) return state
 
             const dayIndex = state.interactiveCalendar[monthIndex].days.findIndex(
               ({ date }) => date === selectedDate
-            );
+            )
 
-            if (dayIndex === -1) return state;
+            if (dayIndex === -1) return state
 
-            const newInteractiveCalendar = [...state.interactiveCalendar];
+            const newInteractiveCalendar = [...state.interactiveCalendar]
             const updatedDay = {
               ...newInteractiveCalendar[monthIndex].days[dayIndex],
-              events: newInteractiveCalendar[monthIndex].days[dayIndex].events.map(
-                (e) => (e.id === tempId ? newEvent : e)
+              events: newInteractiveCalendar[monthIndex].days[dayIndex].events.map((e) =>
+                e.id === tempId ? newEvent : e
               ),
-            };
+            }
 
-            newInteractiveCalendar[monthIndex].days[dayIndex] = updatedDay;
+            newInteractiveCalendar[monthIndex].days[dayIndex] = updatedDay
 
-            return { interactiveCalendar: newInteractiveCalendar };
-          });
+            return { interactiveCalendar: newInteractiveCalendar }
+          })
         } catch (error) {
-          console.error("Failed to create event:", error);
+          console.error('Failed to create event:', error)
           // Revert the optimistic update
-          get().deleteEvent(tempId, selectedMonth);
+          get().deleteEvent(tempId, selectedMonth)
         }
       },
     }),
@@ -133,4 +133,4 @@ export const useCalendarStore = create<CalendarState>()(
       name: 'calendar-store',
     }
   )
-);
+)
